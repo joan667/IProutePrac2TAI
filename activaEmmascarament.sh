51b0ff41aaaa0fde81bc85878099a600074de509
joan667 <44604896+joan667@users.noreply.github.com>
2025-03-24 19:49:18 +0100

    Create activaEmmascarament.sh

---
+++ activaEmmascarament.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+//per el router
+iptables -t nat -A POSTROUTING -s 192.168.2.32/27 -j MASQUERADE
+iptables -t nat -A POSTROUTING -s 192.168.2.64/27 -j MASQUERADE
+iptables -t nat -A POSTROUTING -s 192.168.2.96/27 -j MASQUERADE
+iptables -t nat -A POSTROUTING -s 192.168.2.128/27 -j MASQUERADE
+iptables -t nat -A POSTROUTING -s 192.168.2.160/27 -j MASQUERADE
+iptables -t nat -A POSTROUTING -s 192.168.2.192/27 -j MASQUERADE
+iptables -t nat -A POSTROUTING -s 192.168.2.224/27 -j MASQUERADE
+
+
