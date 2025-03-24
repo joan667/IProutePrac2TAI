34601ac360d4ada246155b1a247fae098a419b50
joan667 <44604896+joan667@users.noreply.github.com>
2025-03-24 20:23:35 +0100

    Update canviaBalanceig.sh

--- canviaBalanceig.sh
+++ canviaBalanceig.sh
@@ -8,3 +8,8 @@ P2=$2
 
 ip route replace default scope global nexthop via 192.168.0.1 dev eth0 weight $P1 \
 nexthop via 192.168.1.1 dev eth1 weight $P2
+
+#Finalmente mostramos el contenido de las tablas de routing
+
+clear
+ip route
