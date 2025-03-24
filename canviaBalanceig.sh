e8aa67553467fd287f2ee4b19ac69856177436fc
joan667 <44604896+joan667@users.noreply.github.com>
2025-03-24 20:20:47 +0100

    Create canviaBalanceig.sh

---
+++ canviaBalanceig.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#Esto es para el router
+
+#TODO a verificar que los pesos son numeros
+
+P1=$1
+P2=$2
+
+ip route replace default scope global nexthop via 192.168.0.1 dev eth0 weight $P1 \
+nexthop via 192.168.1.1 dev eth1 weight $P2
