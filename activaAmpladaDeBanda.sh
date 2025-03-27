2790dfad817b9908ece4ee1246ea256984c71998
joan667 <44604896+joan667@users.noreply.github.com>
2025-03-27 19:05:17 +0100

    Update activaAmpladaDeBanda.sh

--- activaAmpladaDeBanda.sh
+++ activaAmpladaDeBanda.sh
@@ -24,37 +24,37 @@ tc qdisc add dev eth1 root handle 1: prio bands 8
 
 #asignamos las colas a la tipologia TBF
 
-tc qdisc add dev eth0 parent 1:1 handle 20: tbf rate 10mbit latency 50ms burst 1540
+tc qdisc add dev eth0 parent 1:1 handle 10: tbf rate 10mbit latency 50ms burst 1540
 tc qdisc add dev eth0 parent 1:2 handle 20: tbf rate 10kbit latency 50ms burst 1540
-tc qdisc add dev eth0 parent 1:3 handle 20: tbf rate 10kbit latency 50ms burst 1540
-tc qdisc add dev eth0 parent 1:4 handle 20: tbf rate 10kbit latency 50ms burst 1540
-tc qdisc add dev eth0 parent 1:5 handle 20: tbf rate 10kbit latency 50ms burst 1540
-tc qdisc add dev eth0 parent 1:6 handle 20: tbf rate 10kbit latency 50ms burst 1540
-tc qdisc add dev eth0 parent 1:7 handle 20: tbf rate 10kbit latency 50ms burst 1540
-tc qdisc add dev eth0 parent 1:8 handle 20: tbf rate 10kbit latency 50ms burst 1540
+tc qdisc add dev eth0 parent 1:3 handle 30: tbf rate 10kbit latency 50ms burst 1540
+tc qdisc add dev eth0 parent 1:4 handle 40: tbf rate 10kbit latency 50ms burst 1540
+tc qdisc add dev eth0 parent 1:5 handle 50: tbf rate 10kbit latency 50ms burst 1540
+tc qdisc add dev eth0 parent 1:6 handle 60: tbf rate 10kbit latency 50ms burst 1540
+tc qdisc add dev eth0 parent 1:7 handle 70: tbf rate 10kbit latency 50ms burst 1540
+tc qdisc add dev eth0 parent 1:8 handle 80: tbf rate 10kbit latency 50ms burst 1540
 
-tc qdisc add dev eth1 parent 1:1 handle 20: tbf rate 10mbit latency 50ms burst 1540
+tc qdisc add dev eth1 parent 1:1 handle 10: tbf rate 10mbit latency 50ms burst 1540
 tc qdisc add dev eth1 parent 1:2 handle 20: tbf rate 10kbit latency 50ms burst 1540
-tc qdisc add dev eth1 parent 1:3 handle 20: tbf rate 10kbit latency 50ms burst 1540
-tc qdisc add dev eth1 parent 1:4 handle 20: tbf rate 10kbit latency 50ms burst 1540
-tc qdisc add dev eth1 parent 1:5 handle 20: tbf rate 10kbit latency 50ms burst 1540
-tc qdisc add dev eth1 parent 1:6 handle 20: tbf rate 10kbit latency 50ms burst 1540
-tc qdisc add dev eth1 parent 1:7 handle 20: tbf rate 10kbit latency 50ms burst 1540
-tc qdisc add dev eth1 parent 1:8 handle 20: tbf rate 10kbit latency 50ms burst 1540
+tc qdisc add dev eth1 parent 1:3 handle 30: tbf rate 10kbit latency 50ms burst 1540
+tc qdisc add dev eth1 parent 1:4 handle 40: tbf rate 10kbit latency 50ms burst 1540
+tc qdisc add dev eth1 parent 1:5 handle 50: tbf rate 10kbit latency 50ms burst 1540
+tc qdisc add dev eth1 parent 1:6 handle 60: tbf rate 10kbit latency 50ms burst 1540
+tc qdisc add dev eth1 parent 1:7 handle 70: tbf rate 10kbit latency 50ms burst 1540
+tc qdisc add dev eth1 parent 1:8 handle 80: tbf rate 10kbit latency 50ms burst 1540
 
 #añadimos los filtros 
 tc filter add dev eth0 parent 1: protocol ip prio 1 route from 1 flowid 1:2
-tc filter add dev eth0 parent 1: protocol ip prio 1 route from 1 flowid 1:3
-tc filter add dev eth0 parent 1: protocol ip prio 1 route from 1 flowid 1:4
-tc filter add dev eth0 parent 1: protocol ip prio 1 route from 1 flowid 1:5
-tc filter add dev eth0 parent 1: protocol ip prio 1 route from 1 flowid 1:6
-tc filter add dev eth0 parent 1: protocol ip prio 1 route from 1 flowid 1:7
-tc filter add dev eth0 parent 1: protocol ip prio 1 route from 1 flowid 1:8
+tc filter add dev eth0 parent 1: protocol ip prio 1 route from 2 flowid 1:3
+tc filter add dev eth0 parent 1: protocol ip prio 1 route from 3 flowid 1:4
+tc filter add dev eth0 parent 1: protocol ip prio 1 route from 4 flowid 1:5
+tc filter add dev eth0 parent 1: protocol ip prio 1 route from 5 flowid 1:6
+tc filter add dev eth0 parent 1: protocol ip prio 1 route from 6 flowid 1:7
+tc filter add dev eth0 parent 1: protocol ip prio 1 route from 7 flowid 1:8
 
 tc filter add dev eth1 parent 1: protocol ip prio 1 route from 1 flowid 1:2
-tc filter add dev eth1 parent 1: protocol ip prio 1 route from 1 flowid 1:3
-tc filter add dev eth1 parent 1: protocol ip prio 1 route from 1 flowid 1:4
-tc filter add dev eth1 parent 1: protocol ip prio 1 route from 1 flowid 1:5
-tc filter add dev eth1 parent 1: protocol ip prio 1 route from 1 flowid 1:6
-tc filter add dev eth1 parent 1: protocol ip prio 1 route from 1 flowid 1:7
-tc filter add dev eth1 parent 1: protocol ip prio 1 route from 1 flowid 1:8
+tc filter add dev eth1 parent 1: protocol ip prio 1 route from 2 flowid 1:3
+tc filter add dev eth1 parent 1: protocol ip prio 1 route from 3 flowid 1:4
+tc filter add dev eth1 parent 1: protocol ip prio 1 route from 4 flowid 1:5
+tc filter add dev eth1 parent 1: protocol ip prio 1 route from 5 flowid 1:6
+tc filter add dev eth1 parent 1: protocol ip prio 1 route from 6 flowid 1:7
+tc filter add dev eth1 parent 1: protocol ip prio 1 route from 7 flowid 1:8
