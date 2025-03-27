9b992bd3b7d0f0d535f3122c586156e671ced6bf
joan667 <44604896+joan667@users.noreply.github.com>
2025-03-27 19:25:07 +0100

    Update resetAmpladaDeBandaRouter.sh
    sdvsdv

--- resetAmpladaDeBandaRouter.sh
+++ resetAmpladaDeBandaRouter.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 
+#ponemos todos los paquetes no marcados que passen por la cola 1, es decir la que tiene el ancho de banda maximo
+tc qdisc change dev eth0 root handle 1: prio priomap 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
