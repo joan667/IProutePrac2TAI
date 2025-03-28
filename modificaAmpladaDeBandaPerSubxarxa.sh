6d3791411334989241f27f74b55c33b38e4f007f
joan667 <44604896+joan667@users.noreply.github.com>
2025-03-28 09:02:13 +0100

    Update modificaAmpladaDeBandaPerSubxarxa.sh

--- modificaAmpladaDeBandaPerSubxarxa.sh
+++ modificaAmpladaDeBandaPerSubxarxa.sh
@@ -26,7 +26,7 @@ VALOR=${SUBNET_MAP[$1]}
 # Buscar el valor asociado a la subred dada
 
 #SE PODRIA VERFICAR LA BANDA
-A_Banda =$2
+A_Banda=$2
 
-tc qdisc change dev eth0 parent 1:$VALOR handle 20: tbf rate $A_Banda latency 50ms burst 1540
-tc qdisc change dev eth1 parent 1:$VALOR handle 20: tbf rate $A_Banda latency 50ms burst 1540
+tc qdisc change dev eth0 parent 1:$VALOR handle ${VALOR}: tbf rate $A_Banda latency 50ms burst 1540
+tc qdisc change dev eth1 parent 1:$VALOR handle ${VALOR}: tbf rate $A_Banda latency 50ms burst 1540
